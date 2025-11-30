baef0951a521f7085631b2fd1f70b06b3f42bed5
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2025-11-30 18:33:15 -0500

    Class, File and method comments for Dashboard page cs file is done

--- GroundTerminalApp/SearchingPageApp.xaml.cs
+++ GroundTerminalApp/SearchingPageApp.xaml.cs
@@ -263,14 +263,16 @@ namespace GroundTerminalApp
 
         private void LiveUpdateTelemetryLabel()
         {
+            // this will check the value if its null will return and the UI will display the default 
             var latestPacket = dashboard.telemetryDataList.LastOrDefault();
             if (latestPacket == null) return;
 
+            // if we get the live value of the data from the dashboard we will update the text boxes and labels 
             QueryStatusChip.Text = "Live Data Ready";
 
             ResultsHeaderLbl.Content = " Reciving a value of 6 Values";
 
-            // create a new display object with formatted strings
+            // create a new display object with formatted strings to display in the binding object on xml 
             var displayValues = new TelemetryDataNameAndValues
             {
 
@@ -288,12 +290,6 @@ namespace GroundTerminalApp
 
 
 
-
-        private double NormalizeAltitude(double altitude)
-        {
-            double maxAltitude = 40000; 
-            return (altitude / maxAltitude) * 140;
-        }
 
 
         /*  
